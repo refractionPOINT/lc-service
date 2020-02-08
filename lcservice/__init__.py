3b2e691dcff123e0ac364099e1f821400539e79e
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2020-02-08 09:52:05 -0800

    Version bump.

--- lcservice/__init__.py
+++ lcservice/__init__.py
@@ -1,6 +1,6 @@
 """Reference implementation for LimaCharlie.io services."""
 
-__version__ = "1.3.1"
+__version__ = "1.4.0"
 __author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
 __author_email__ = "maxime@refractionpoint.com"
 __license__ = "Apache v2"
