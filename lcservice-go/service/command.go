57dcd75624053f911bcd5904dcd4725a1a339e05
Francis Mireault <mireaulf@gmail.com>
2021-02-24 16:46:25 -0500

    Add builder func

--- lcservice-go/service/command.go
+++ lcservice-go/service/command.go
@@ -1,5 +1,9 @@
 package service
 
+import (
+	"fmt"
+)
+
 type CommandNamespace = string
 type CommandName = string
 type Command struct {
@@ -8,42 +12,44 @@ type Command struct {
 	RequestParams RequestParams    `json:"params"`
 }
 
-func NewCommand(namespace CommandNamespace) *Command {
-	return &Command{Namespace: namespace}
+func NewCommand(namespace CommandNamespace) (*Command, error) {
+	return &Command{Namespace: namespace}, nil
 }
 
-func (c *Command) WithName(name CommandName) *Command {
+func (c *Command) WithName(name CommandName) error {
 	c.Name = name
-	return c
+	return nil
 }
 
-func (c *Command) WithParam(name RequestParamName, desc string, paramType RequestParamType, required bool, values []string) *Command {
+func (c *Command) AddParam(name RequestParamName, desc string, paramType RequestParamType, required bool, values []string) error {
+	if _, found := allowedRequestParamTypes[paramType]; !found {
+		return fmt.Errorf("unsupported request parameter type %s", paramType)
+	}
 	paramValues := []string{}
 	if paramType == RequestParamTypeEnum {
 		paramValues = values
 	}
-
 	c.RequestParams[name] = RequestParamDef{
 		Type:        paramType,
 		Description: desc,
 		IsRequired:  required,
 		Values:      paramValues,
 	}
-	return c
+	return nil
 }
 
-func (c *Command) WithParamEnumOptional(name RequestParamName, desc string, values []string) *Command {
-	return c.WithParam(name, desc, RequestParamTypeEnum, false, values)
+func (c *Command) AddParamEnumOptional(name RequestParamName, desc string, values []string) error {
+	return c.AddParam(name, desc, RequestParamTypeEnum, false, values)
 }
 
-func (c *Command) WithParamEnumRequired(name RequestParamName, desc string, values []string) *Command {
-	return c.WithParam(name, desc, RequestParamTypeEnum, true, values)
+func (c *Command) AddParamEnumRequired(name RequestParamName, desc string, values []string) error {
+	return c.AddParam(name, desc, RequestParamTypeEnum, true, values)
 }
 
-func (c *Command) WithParamOptional(name RequestParamName, desc string, paramType RequestParamType) *Command {
-	return c.WithParam(name, desc, paramType, false, nil)
+func (c *Command) AddParamOptional(name RequestParamName, desc string, paramType RequestParamType) error {
+	return c.AddParam(name, desc, paramType, false, nil)
 }
 
-func (c *Command) WithParamRequired(name RequestParamName, desc string, paramType RequestParamType) *Command {
-	return c.WithParam(name, desc, paramType, true, nil)
+func (c *Command) AddParamRequired(name RequestParamName, desc string, paramType RequestParamType) error {
+	return c.AddParam(name, desc, paramType, true, nil)
 }
