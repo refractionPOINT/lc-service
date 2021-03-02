697e2a7a70d4657cf53a011000fd8427d0ee2cde
Francis Mireault <mireaulf@gmail.com>
2021-03-02 16:09:23 -0500

    Add endpoint to process command

--- lcservice-go/servers/iface.go
+++ lcservice-go/servers/iface.go
@@ -3,4 +3,5 @@ package servers
 type Service interface {
 	Init() error
 	ProcessRequest(data map[string]interface{}, sig string) (response interface{}, isAccepted bool)
+	ProcessCommand(commandArguments map[string]interface{}, sig string) (response interface{}, isAccepted bool)
 }
