52d6eff829ab977020bb6c88cf4894fcc5b468ce
Francis Mireault <mireaulf@gmail.com>
2021-03-08 10:50:35 -0500

    Make process public (#7)

--- lcservice-go/servers/cloud_function.go
+++ lcservice-go/servers/cloud_function.go
@@ -18,6 +18,6 @@ func (cf *CloudFunction) Init() error {
 	return cf.svc.Init()
 }
 
-func (cf *CloudFunction) process(w http.ResponseWriter, r *http.Request) {
+func (cf *CloudFunction) Process(w http.ResponseWriter, r *http.Request) {
 	process(cf.svc, w, r)
 }
