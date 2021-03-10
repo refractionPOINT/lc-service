6e8eb3a9ed531ff1275e4f793bc67eef0ea7a281
Francis Mireault <mireaulf@gmail.com>
2021-03-10 15:43:20 -0500

    Parse on requestType and assume request if missing (#9)
    * Parse on requestType and assume request if missing
    
    * Use same naming scheme
    
    * Print expected sig

--- lcservice-go/servers/common.go
+++ lcservice-go/servers/common.go
@@ -15,6 +15,17 @@ func isRequestNotImplemented(resp interface{}) bool {
 	return svcResp.Error == svc.ErrNotImplemented.Error
 }
 
+func handleResponse(resp interface{}, isAccepted bool, w http.ResponseWriter) {
+	if !isAccepted {
+		w.WriteHeader(401)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		w.WriteHeader(500)
+		return
+	}
+}
+
 func process(service Service, w http.ResponseWriter, r *http.Request) {
 	d := map[string]interface{}{}
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
@@ -23,18 +34,16 @@ func process(service Service, w http.ResponseWriter, r *http.Request) {
 	}
 	sig := r.Header.Get("lc-svc-sig")
 
-	resp, isAccepted := service.ProcessRequest(d, sig)
-	if !isAccepted {
-		w.WriteHeader(401)
+	requestTypeValue, ok := d["request_type"]
+	if !ok || requestTypeValue == "request" {
+		// it's not there we assume it's a regular request
+		resp, isAccepted := service.ProcessRequest(d, sig)
+		handleResponse(resp, isAccepted, w)
 		return
 	}
-
-	if isRequestNotImplemented(resp) {
-		resp, isAccepted = service.ProcessCommand(d, sig)
-	}
-
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		w.WriteHeader(500)
+	if requestTypeValue == "command" {
+		resp, isAccepted := service.ProcessCommand(d, sig)
+		handleResponse(resp, isAccepted, w)
 		return
 	}
 }
